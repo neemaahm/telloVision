551ed84d5bc1f4c7793132c340c62384dd7cf9ad
Neema Ahmadian <neemaahm@gmail.com>
2022-07-19 12:11:40 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from djitellopy import Tello
+import pygame
+import controls
+import myGUI
+
+myGUI = myGUI.myGUI()
+controls = controls.controls()
+
+tello = Tello()
+print("hello")
+controls.connect_drone(tello)
+
+print("Tello Battery: " + str(tello.get_battery()))
+while controls.do_loop:
+    pygame.event.pump()
+    #myGUI.doVideoFeed(tello)
+    controls.take_inputs()
+    controls.check_shutdown(tello)
+    myGUI.drawRectangles(controls.stick_values)
+    controls.move_tello(tello, 60, 90, 20)
+
+    pygame.display.update()
+    myGUI.clock.tick(60)
---
+++ videoFeedOnly.py
@@ -0,0 +1,13 @@
+import cv2
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+
+tello.streamon()
+
+while True:
+    frame_read = tello.get_frame_read()
+    cv2.imshow("picture", frame_read.frame)
+
+tello.streamoff()
