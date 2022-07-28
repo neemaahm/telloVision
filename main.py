e957e883b581d34ca0fc3bfb639ddfaaa453d9b3
neemaahm <neemaahm@gmail.com>
2022-07-28 14:37:55 -0400

    Managed some files and tested TrackerState.py

--- main.py
+++ main.py
@@ -3,24 +3,33 @@
 import numpy as np
 import Coordinates
 import DroneState
+import TrackerState
 from djitellopy import Tello
 import time
 
+g0_r = np.array([[2.6030], [-2.4606], [-1.6157]])
+gxa_r = np.array([[0.3805], [-5.3250], [-1.5978]])
 
-Coords = Coordinates.Coordinates()
-Coords.init_global_mission_pad(1, np.array([[1], [1], [0]]))
-Coords.init_global_mission_pad(7, np.array([[2], [2], [0]]))
+Tracker = TrackerState.TrackerState(g0_r, gxa_r)
+print(Tracker.get_tracker_pos())
+time.sleep(5)
+print(Tracker.get_tracker_pos())
 
-drone_0_g = np.array([[1],[1],[0]])
-myDrone = DroneState.DroneState(drone_0_g)
-tello = Tello()
 
-myDrone.connect_drone(tello)
+# Coords = Coordinates.Coordinates()
+# Coords.init_global_mission_pad(1, np.array([[1], [1], [0]]))
+# Coords.init_global_mission_pad(7, np.array([[2], [2], [0]]))
 
-p_g = np.array([[2.5], [2], [1]])
+# drone_0_g = np.array([[1],[1],[0]])
+# myDrone = DroneState.DroneState(drone_0_g)
+# tello = Tello()
 
-# myDrone.move_xyz_relative(tello, Coords, 1, 1, 0)
-myDrone.move_xyz_global(tello, Coords, p_g)
-print(myDrone.drone_state)
+# myDrone.connect_drone(tello)
 
-myDrone.drone_shutdown(tello)
+# p_g = np.array([[2.5], [2], [1]])
+
+# # myDrone.move_xyz_relative(tello, Coords, 1, 1, 0)
+# myDrone.move_xyz_global(tello, Coords, p_g)
+# print(myDrone.drone_state)
+
+# myDrone.drone_shutdown(tello)
--- tracker_test.py
+++ tracker_test.py
@@ -8,7 +8,8 @@ import Coordinates
 g0_r = np.array([[2.6030], [-2.4606], [-1.6157]])
 gxa_r = np.array([[0.3805], [-5.3250], [-1.5978]])
 
-Coords = Coordinates.Coordinates(g0_r, gxa_r)
+Coords = Coordinates.Coordinates()
+Coords.init_global_tracker(g0_r, gxa_r)
 
 v = triad_openvr.triad_openvr()
 v.print_discovered_objects()
@@ -32,7 +33,7 @@ if interval:
         
         pose_r = v.devices["tracker_1"].get_pose_euler()
         tracker_pos_r = np.array([[pose_r[0]], [pose_r[2]], [pose_r[1]]])
-        tracker_pos_g = Coords.global_pos(tracker_pos_r)
+        tracker_pos_g = Coords.global_pos_tracker(tracker_pos_r)
         txt_global = "  |  Global Values: "
         txt_global += "%.4f" % tracker_pos_g[0] + " "
         txt_global += "%.4f" % tracker_pos_g[1] + " "
